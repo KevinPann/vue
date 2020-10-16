fefb0b62bd9d93f499ffb62ea38376444c7e1304
KevinPann <36241579+KevinPann@users.noreply.github.com>
2020-10-16 10:51:49 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
             colors: {
                 'navy-500': '#171a21',
                 'green-steam': '#5c7e10',
-                'secondaryBlue': 'rgba(62, 126, 167, 0.8)'
+                'secondaryBlue': 'rgba(62, 126, 167, 0.8)',
+                'searchbox': '#316282',
+                'search': '#0e1c25'
+            },
+            backgroundImage: {
+                'hero-pattern': "url('https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg/220px-Ash_Tree_-_geograph.org.uk_-_590710.jpg')"
             }
         },
     },
