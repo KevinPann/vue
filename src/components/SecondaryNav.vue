9d1e93252cbcc1dd381da4def2d03b8d46a5909d
KevinPann <36241579+KevinPann@users.noreply.github.com>
2020-10-16 10:52:37 +0300

    Update SecondaryNav.vue

--- src/components/SecondaryNav.vue
+++ src/components/SecondaryNav.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="flex flex-1 bg-secondaryBlue mx-40">
-    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-blue-800">
-      <span class="text-white">Sinu Pood</span>
+    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-white">
+      <span class="text-gray-400">Sinu Pood</span>
     </div>
-    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-blue-800">
-      <span class="text-white">Sirvi</span>
+    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-white">
+      <span class="text-gray-400">Sirvi</span>
     </div>
-    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-blue-800">
-      <span class="text-white">Punktide Pood</span>
+    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-white">
+      <span class="text-gray-400">Punktide Pood</span>
     </div>
-    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-blue-800">
-      <span class="text-white">Uudised</span>
+    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-white">
+      <span class="text-gray-400">Uudised</span>
     </div>
-    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-blue-800">
-      <span class="text-white">Labor</span>
+    <div class="px-4 py-4 border-r-2 border-navy-500 hover:bg-white">
+      <span class="text-gray-400">Labor</span>
     </div>
     <div class="ml-auto p-4">
-      <input type="text" name="search" />
+      <input
+        type="text"
+        name="search"
+        class="bg-searchbox text-search border-inset border-solid 1px bg-none h-11"
+        value="Type"
+      />
     </div>
   </div>
 </template>
